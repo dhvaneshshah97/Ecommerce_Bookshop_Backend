5fb7f7b300748516a498898bffd82e7ab37795e5
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-03 16:40:41 -0700

    Implement Update and Delete Operation on Product

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,24 @@ const _ = require('lodash');
 const Product = require('../models/product.js');
 const fs = require('fs');
 
+exports.productById = (req,res,next,id) => {
+    Product.findById(id , (err, product) => {
+        if(err || !product) {
+            return res.status(400).json({
+                error: "Product not found!",
+            });
+        }
+
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm(); // all the form data will be available from this IncomingForm
     form.keepExtensions = true // whatever image type we will be getting, the extensions will be there.
@@ -46,6 +64,71 @@ exports.create = (req, res) => {
                 });
             }
             res.json(result);
+        })
+
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                err,
+            });
+        }
+        res.json({
+            message: "Product deleted successfully",
+        }); 
+    })
+}
+
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm(); // all the form data will be available from this IncomingForm
+    form.keepExtensions = true // whatever image type we will be getting, the extensions will be there.
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded",
+            });
+        }
+        //check for all fields
+        // const { name, description, price, quantity, shipping, category } = fields
+        // if (!name || !description || !price || !quantity || !category || !shipping) {
+        //     return res.status(400).json({
+        //         error: "All fields are required!",
+        //     });
+        // }
+        // Now, we have changes, so we need to replace the existing information with new information, for that we will use extend method of lodash library
+        let product = req.product
+        // extend method takes 2 arguments, first it takes product and then changes
+        product = _.extend(product, fields)
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        // handling files
+        if (files.photo) {
+            console.log('File photo: ', files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size",
+                });
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    err
+                });
+            }
+            res.json({
+                message: "Product Updated Successfully",
+            });
         })
 
     });
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin  } = require('../controllers/auth.js');
-const { create } = require('../controllers/product.js');
+const { create, productById, read, remove, update } = require('../controllers/product.js');
 const { userById } = require('../controllers/user.js');
 
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+//put method is used to update the product
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+
+
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
 
