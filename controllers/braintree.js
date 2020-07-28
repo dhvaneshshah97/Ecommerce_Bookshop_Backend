b1ce0371287c7f1d26cbc4dc28eb61d97f64f86e
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-07-27 18:20:03 -0700

    Add an API processPayment

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -21,3 +21,22 @@ exports.generateToken = (req, res) => {
         }
     })
 }
+
+exports.processPayment = (req, res) => {
+    let nonceFromTheClient = req.body.paymentMethodNonce;
+    let amountFromTheClient = req.body.amount;
+    // charge 
+    let newTransaction = gateway.transaction.sale({
+        amount: amountFromTheClient,
+        paymentMethodNonce: nonceFromTheClient,
+        options: {
+            submitForSettlement: true,
+        },
+    }, (error, result) => {
+        if (error) {
+            res.status(500).json(error);
+        } else {
+            res.json(result);
+        }
+    });
+}
--- routes/braintree.js
+++ routes/braintree.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAuth } = require('../controllers/auth.js');
 const { userById } = require('../controllers/user.js');
-const { generateToken } = require('../controllers/braintree.js');
+const { generateToken, processPayment } = require('../controllers/braintree.js');
 
 
 router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken);
+router.post('/braintree/payment/:userId', requireSignin, isAuth, processPayment);
 
 router.param("userId", userById);
 
