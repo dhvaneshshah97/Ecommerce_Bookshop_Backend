90b549697aab7c44674f79addfe16770eef7fbc3
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-01 19:45:54 -0700

    Add return statement in create method of order handler

--- controllers/order.js
+++ controllers/order.js
@@ -1,3 +1,5 @@
 exports.create = (req, res) => {
     console.log("Create order", req.body);
+    // we have to return something, so that execution does not halt at await and code after await works.
+    return res.json({});
 }
