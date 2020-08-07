50c19fce66e9671f5fc73c2f12884809ffdcee6e
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-06 19:37:30 -0700

    Add handler to save order details into the database

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,20 @@
+const {Order, CartItem} = require('../models/order');
+
+
 exports.create = (req, res) => {
-    console.log("Create order", req.body);
+    // console.log("Create order", req.body);
+    req.body.order.user = req.profile
+    const order = new Order(req.body.order);
+    order.save((error, data) => {
+        if (error) {
+            return res.status(400).json({
+                error: error,
+            });
+        }
+        res.json(data);
+    })
+
+
     // we have to return something, so that execution does not halt at await and code after await works.
     return res.json({});
 }
