49b13a9ec9500ee43ef48b832746fba82ddcd9b5
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-23 11:33:00 -0700

    Update sold products' quantity

--- routes/order.js
+++ routes/order.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { requireSignin, isAuth } = require('../controllers/auth.js');
 const { userById, addOrderToUserHistory } = require('../controllers/user.js');
 const { create } = require('../controllers/order.js');
+const { decreaseQuantity } = require('../controllers/product.js');
 
-router.post("/order/create/:userId", requireSignin, isAuth, addOrderToUserHistory, create);
+router.post("/order/create/:userId", requireSignin, isAuth, addOrderToUserHistory, decreaseQuantity, create);
 
 router.param("userId", userById);
 
