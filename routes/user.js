2338f43e995a9bb9a8dcdad5e8417b86ba8ef87b
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-26 17:04:56 -0700

    Add comments

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,8 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     res.json(req.profile);
 });
 
-router.get("/user/:userId", requireSignin, isAuth, read);
-router.put("/user/:userId", requireSignin, isAuth, update);
+router.get("/user/:userId", requireSignin, isAuth, read);       // get the user information
+router.put("/user/:userId", requireSignin, isAuth, update);     // updates user profile
 
 // whenever this userId is in URL, below line will run and as a consequence, userById method will run, which stores current user(whose id is in URL bar) in req.profile object.
 router.param("userId", userById);
